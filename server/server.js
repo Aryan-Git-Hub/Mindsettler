5b4559534f165769ed84a9d57e17b101044a30b0
Aryan-Git-Hub <aryan.hmt1234@gmail.com>
2025-12-23 17:14:07 +0530

    mongodb connections successfull

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 import app from "./app.js";
+import 'dotenv/config';
+import connectDB from "./config/db.js";
 
-const PORT = 4000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// Connect to Database
+connectDB();
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
