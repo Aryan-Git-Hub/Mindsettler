4b4fe74b75af049fb5a217c452c9e59248173f09
Jay-CodeHube01 <pateljay18v@email.com>
2025-12-25 23:12:44 +0530

    Add navvar only at perticular pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,39 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Ensure it's 'react-router-dom'
+import { BrowserRouter, Route, Routes, Outlet } from 'react-router-dom';
 import Home from './pages/Home';
 import PageNotFound from './pages/404';
-import ScrollToTop from "./components/common/ScrollToTop";
-import Navbar from './components/common/Navbar';
-import AuthPage from "./pages/login.jsx"
-
-
 import AdminDashboard from './admin/Dashboard';
+import ScrollToTop from "./components/common/ScrollToTop";
+import AuthPage from "./pages/login.jsx";
+import Navbar from "./components/common/Navbar.jsx"; // Assuming path
+
+// A small component to wrap public pages with the Navbar
+const PublicLayout = () => (
+  <>
+    <Navbar />
+    <Outlet /> {/* This is where Home, Login, etc. will render */}
+  </>
+);
 
 function App() {
   return (
     <BrowserRouter>
       <ScrollToTop />
-      <Navbar />
       <Routes>
-        {/* Public Routes */}
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<AuthPage />} />
+        
+        {/* GROUP 1: Public Pages (With Navbar) */}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<AuthPage />} />
+          {/* Add other public pages like /about or /contact here */}
+        </Route>
 
-        {/* Admin Routes with Shared Layout */}
+        {/* GROUP 2: Admin Pages (No Public Navbar) */}
+        {/* Your AdminDashboard has its own Sidebar, so it doesn't need a wrapper */}
         <Route path="/admin" element={<AdminDashboard />} />
 
         {/* 404 Route */}
         <Route path="*" element={<PageNotFound />} />
+        
       </Routes>
     </BrowserRouter>
   );
